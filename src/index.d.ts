7d655fedff077d0b0f25369e6317f2eaafe42f79
OPADA-Eng <m.opada.albosh@gmail.com>
2020-02-12 12:15:12 +0200

    first commit

---
+++ src/index.d.ts
@@ -0,0 +1,6 @@
+import { Common } from './woosim-printer-i350.common';
+export declare class WoosimPrinterI350 extends Common {
+  // define your typings manually
+  // or..
+  // take the ios or android .d.ts files and copy/paste them here
+}
---
+++ src/woosim-printer-i350.android.ts
@@ -0,0 +1,5 @@
+import { Common } from './woosim-printer-i350.common';
+
+export class WoosimPrinterI350 extends Common {
+
+}
